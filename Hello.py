bce31cfdb4251b879364ea13917e644c28baa316
ryps2022 <111885932+ryps2022@users.noreply.github.com>
2024-03-04 12:06:30 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -44,29 +44,46 @@ def main():
     st.markdown("""
     This app performs similarity matching between two sets of URLs for the purpose of URL redirection mapping. 
     Please follow these instructions:
-    - **Step 1: Crawl your live website with Screaming Frog**
-    You’ll need to perform a standard crawl on your website. Depending on how your website is built, this may or may not require a JavaScript crawl. The goal is to produce a list of as many accessible pages on your site as possible.
 
-    - **Step 2: Export HTML pages with 200 Status Code**
-    Once the crawl has been completed, we want to export all of the found HTML URLs with a 200 Status Code. This will provide you with a list of our current live URLs and all of the default metadata Screaming Frog collects about them, such as Titles and Header Tags. Save this file as origin.csv.
-
-    - **Step 3: Repeat steps 1 and 2 for your staging website**
-    We now need to gather the same data from our staging website, so we have something to compare to.
-    Depending on how your staging site is secured, you may need to use features such as Screaming Frog’s forms authentication if password protected.
-    Once the crawl has completed, you should export the data and save this file as destination.csv.
-
-    - **Optional: Find and replace your staging site domain or subdomain to match your live site**
-    It’s likely your staging website is either on a different subdomain, TLD or even domain that won’t match our actual destination URL. For this reason, I will use a Find and Replace function on my destination.csv to change the path to match the final live site subdomain, domain or TLD.
-    For example:
-    The live website is https://examplesite.com/ (origin.csv)
-    The staging website is https://test.examplesite.dev/ (destination.csv)
-    The site is staying on the same domain; it’s just a redesign with different URLs, so I would open origin.csv and find any instance of https://examplesite.com/ and replace it with https://test.examplesite.dev/
-    Find and Replace in Excel
-    This also means when the redirect map is produced, the output is correct and only the final redirect logic needs to be written.
+    ---    
     """)
+    
+    # Create a two-column layout
+    col1, col2 = st.beta_columns([2, 1])
 
-    origin_file = st.file_uploader("Upload origin.csv", type=['csv'], help="Please upload the CSV file containing the origin URLs")
-    destination_file = st.file_uploader("Upload destination.csv", type=['csv'], help="Please upload the CSV file containing the destination URLs")
+    # Instructions in the left column
+    with col1:
+        st.markdown("""
+        **Step 1: Crawl your live website with Screaming Frog**
+
+        You’ll need to perform a standard crawl on your website. Depending on how your website is built, this may or may not require a JavaScript crawl. The goal is to produce a list of as many accessible pages on your site as possible.
+
+        **Step 2: Export HTML pages with 200 Status Code**
+
+        Once the crawl has been completed, we want to export all of the found HTML URLs with a 200 Status Code. This will provide you with a list of our current live URLs and all of the default metadata Screaming Frog collects about them, such as Titles and Header Tags. Save this file as origin.csv.
+
+        **Step 3: Repeat steps 1 and 2 for your staging website**
+
+        We now need to gather the same data from our staging website, so we have something to compare to.
+        Depending on how your staging site is secured, you may need to use features such as Screaming Frog’s forms authentication if password protected.
+        Once the crawl has completed, you should export the data and save this file as destination.csv.
+
+        **Optional: Find and replace your staging site domain or subdomain to match your live site**
+
+        It’s likely your staging website is either on a different subdomain, TLD or even domain that won’t match our actual destination URL. For this reason, I will use a Find and Replace function on my destination.csv to change the path to match the final live site subdomain, domain or TLD.
+        For example:
+        The live website is https://examplesite.com/ (origin.csv)
+        The staging website is https://test.examplesite.dev/ (destination.csv)
+        The site is staying on the same domain; it’s just a redesign with different URLs, so I would open origin.csv and find any instance of https://examplesite.com/ and replace it with https://test.examplesite.dev/
+        Find and Replace in Excel
+        This also means when the redirect map is produced, the output is correct and only the final redirect logic needs to be written.
+        """)
+
+    # Empty space for layout
+    col2.write("")
+
+    origin_file = st.file_uploader("Upload origin.csv", type=['csv'], help="Please upload the CSV file containing the origin URLs", key="origin")
+    destination_file = st.file_uploader("Upload destination.csv", type=['csv'], help="Please upload the CSV file containing the destination URLs", key="destination")
 
     if origin_file is not None and destination_file is not None:
         origin_df = pd.read_csv(origin_file)
